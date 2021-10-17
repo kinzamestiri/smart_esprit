d68bbd9653042da0e790a01a18951e34969cf977
Amirhajji <amir.hajji@esprit.tn>
2021-10-17 15:24:52 +0100

    MAJ doc1

---
+++ doc1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int a,b,somme;
+printf("tapez le premier nombre : \n");
+scanf("%d",&a);
+printf("donnez le deuxieme nombre : \n");
+scanf("%d",&b);
+somme=a+b;
+printf("la somme de ces deux nombres est %d ",somme);
+return 0;
+}
